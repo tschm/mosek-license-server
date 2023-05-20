8ddd1384e993677fd3444a58f51760ab25759e17
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-05-20 08:22:49 -0700

    specify server

--- mosek_license/license.py
+++ mosek_license/license.py
@@ -8,9 +8,9 @@ from urllib.request import urlopen
 ENV_VARNAME = "MOSEKLM_LICENSE_FILE"
 
 
-def upsert():
+def upsert(server=None):
     """Insert or update an environment varible MOSEKLM_LICENSE_FILE"""
-    os.environ[ENV_VARNAME] = _url()
+    os.environ[ENV_VARNAME] = _url(server=server)
 
 
 def current():
@@ -18,8 +18,7 @@ def current():
     return os.environ.get(ENV_VARNAME)
 
 
-def _url():
-    with urlopen(
-        f"http://localhost:8080/mosek"
-    ) as page:
+def _url(server=None):
+    server = server or "http://localhost:8080/mosek"
+    with urlopen(server) as page:
         return page.read().decode("utf-8")
