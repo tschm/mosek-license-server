977614bfb5e8b83dcaa74679408eeacf8e22c5fe
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-05-22 08:18:40 -0700

    black

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo "Exposing the license"
+cat $PWD/license/mosek
+
 PORT=8080
 
 docker run --name mosek -v $PWD/license:/usr/share/nginx/html:ro -p $PORT:80 -d nginx
 
 echo "Nginx server running on port $PORT"
 echo "Stop the server using 'docker rm -f mosek'"
+
+
 
