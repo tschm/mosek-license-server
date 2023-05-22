f5a7f083f7cf9faa19bf4e781b976d26590f36fc
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-05-22 08:08:20 -0700

    rename server starting

--- license_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT=8080
-
-docker run --name mosek -v $PWD/license:/usr/share/nginx/html:ro -p $PORT:80 -d nginx
-
-echo "Nginx server running on port $PORT"
-echo "Stop the server using 'docker rm -f mosek'"
-
