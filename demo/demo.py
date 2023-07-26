90ff218ba7170212d5e78751ba5daf24f1aad34b
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-26 22:41:29 +0400

    Tschm patch 1 (#16)
    * Update .pre-commit-config.yaml
    
    * markdown linting
    
    * establish web/mosek folder
    
    * establish web/mosek folder
    
    * establish web/mosek folder
    
    * fmt
    
    * demo

--- demo/demo.py
+++ demo/demo.py
@@ -7,8 +7,7 @@ from mosek_license import license
 
 license.upsert()
 
-
-if __name__ == "__main__":
+def main():
     with fusion.Model("cqo1") as M:
         # this is optional, just to be safe...
         M.putlicensepath(license.current())
@@ -38,12 +37,12 @@ if __name__ == "__main__":
         qc1 = M.constraint("qc1", z1, fusion.Domain.inQCone())
         qc2 = M.constraint("qc2", z2, fusion.Domain.inRotatedQCone())
 
+
         # Set the objective function to (y[0] + y[1] + y[2])
         M.objective("obj", fusion.ObjectiveSense.Minimize, fusion.Expr.sum(y))
 
         # Solve the problem
         M.solve()
-        M.writeTask("cqo1.ptf")
 
         # Get the linear solution values
         solx = x.level()
@@ -56,3 +55,7 @@ if __name__ == "__main__":
         qc1sn = qc1.dual()
         print("qc1 levels                = %s" % str(qc1lvl))
         print("qc1 dual conic var levels = %s" % str(qc1sn))
+
+if __name__ == "__main__":
+    main()
+
