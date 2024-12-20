a1c2a4f62e4eea6b0d07285707614d5c6fea1e96
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2024-12-20 08:59:47 +0400

    problems in license file

--- tests/test_current.py
+++ tests/test_current.py
@@ -23,3 +23,43 @@ def test_mock(resource_dir):
         # read the file again
         with open(resource_dir / "license") as f:
             assert license == f.read()  # .decode("utf-8")
+
+
+def test_missing_start_license(resource_dir):
+    # https://stackoverflow.com/questions/32043035/python-3-urlopen-context-manager-mocking
+    data = open(resource_dir / "license_p1", "rb")
+
+    with pytest.raises(AssertionError):
+        with patch.object(urllib.request, "urlopen", return_value=data):
+            # give some date to the url function to modify today's date
+            _url(today=datetime.strptime("01-Jan-2025", "%d-%b-%Y").date())
+
+
+def test_missing_end_license(resource_dir):
+    # https://stackoverflow.com/questions/32043035/python-3-urlopen-context-manager-mocking
+    data = open(resource_dir / "license_p2", "rb")
+
+    with pytest.raises(AssertionError):
+        with patch.object(urllib.request, "urlopen", return_value=data):
+            # give some date to the url function to modify today's date
+            _url(today=datetime.strptime("01-Jan-2025", "%d-%b-%Y").date())
+
+
+def test_missing_vendor(resource_dir):
+    # https://stackoverflow.com/questions/32043035/python-3-urlopen-context-manager-mocking
+    data = open(resource_dir / "license_p3", "rb")
+
+    with pytest.raises(AssertionError):
+        with patch.object(urllib.request, "urlopen", return_value=data):
+            # give some date to the url function to modify today's date
+            _url(today=datetime.strptime("01-Jan-2025", "%d-%b-%Y").date())
+
+
+def test_expired_license(resource_dir):
+    # https://stackoverflow.com/questions/32043035/python-3-urlopen-context-manager-mocking
+    data = open(resource_dir / "license", "rb")
+
+    with pytest.raises(AssertionError):
+        with patch.object(urllib.request, "urlopen", return_value=data):
+            # give some date to the url function to modify today's date
+            _url(today=datetime.strptime("01-Jan-2026", "%d-%b-%Y").date())
